2b2585410fab8ff9b2b8f63be125f492316febf3
adriencagle <109845990+adriencagle@users.noreply.github.com>
2022-08-17 19:42:28 -0400

    Add files via upload

---
+++ 10.6 Destructuring.js
@@ -0,0 +1,23 @@
+// 8, 1846
+
+// yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659
+
+// Your name is Alejandro and you like purple, Your name is Melissa and you like green, Your name is undefined and you like green
+
+// first = maya, second = Marisa, Third = Chi
+
+// raindrops = Raindrops on Kittens, whiskers = whiskers on kittens, aFewOfMyFavoriteThings = bright copper kettles, warm woolen mittens, brown paper packages tied up with strings
+
+// [10,30,20]
+
+let obj = {
+    numbers: {
+        a:1,
+        b:2
+    }
+}
+const {a,b} = obj.numbers;
+
+[arr[1], arr[2]] = [arr[2], arr[1]]
+
+const raceResults =  ([first, second, third, ...rest]) => {first, second, third, rest}
